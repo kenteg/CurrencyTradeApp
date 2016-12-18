5f1e488b061174461a583d5f4e38ccbc31d30bfe
kenteg <kenteg91@gmail.com>
2016-12-18 15:34:08 +0600

    created initial app

---
+++ src/main/java/com/luxoft/currencytradeapp/config/Init.java
@@ -0,0 +1,35 @@
+package com.luxoft.currencytradeapp.config;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Декабрь 18 2016
+ */
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class Init implements WebApplicationInitializer {
+
+    // Servlet Dispatcher
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+
+        // Регистрируем в контексте конфигурационный класс, который мы создадим ниже
+        ctx.register(WebAppConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER_SERVLET_NAME,
+                new Init(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+
+}
