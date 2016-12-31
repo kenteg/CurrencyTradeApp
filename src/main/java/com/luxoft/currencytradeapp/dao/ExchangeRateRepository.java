b6e52769d204f69ac9ba26b87b0e747a13a8286c
kenteg <kenteg91@gmail.com>
2017-01-01 01:56:38 +0600

    find all

--- src/main/java/com/luxoft/currencytradeapp/dao/ExchangeRateRepository.java
+++ src/main/java/com/luxoft/currencytradeapp/dao/ExchangeRateRepository.java
@@ -3,9 +3,12 @@ package com.luxoft.currencytradeapp.dao;
 import com.luxoft.currencytradeapp.entity.ExchangeRate;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by omsk16 on 12/30/2016.
  */
 public interface ExchangeRateRepository extends CrudRepository<ExchangeRate,Integer>{
     ExchangeRate findByCurrency1AndCurrency2(String currency1,String currency2);
+    List<ExchangeRate> findAll();
 }
