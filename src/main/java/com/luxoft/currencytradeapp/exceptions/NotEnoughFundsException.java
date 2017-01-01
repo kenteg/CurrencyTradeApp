708be1478aa4b1eb7b1be8557b863cc7bd8c9671
kenteg <kenteg91@gmail.com>
2017-01-01 19:27:37 +0600

    create noEnoughFunds Exception

---
+++ src/main/java/com/luxoft/currencytradeapp/exceptions/NotEnoughFundsException.java
@@ -0,0 +1,11 @@
+package com.luxoft.currencytradeapp.exceptions;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Январь 01 2017
+ */
+public class NotEnoughFundsException extends Exception {
+    public NotEnoughFundsException(String message) {
+        super(message);
+    }
+}
