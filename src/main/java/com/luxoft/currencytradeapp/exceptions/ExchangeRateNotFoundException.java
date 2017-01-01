346079d772f20240c0ec3db15192dd8a1df0b430
kenteg <kenteg91@gmail.com>
2017-01-01 22:31:40 +0600

    add exchangeratenotfoundException

---
+++ src/main/java/com/luxoft/currencytradeapp/exceptions/ExchangeRateNotFoundException.java
@@ -0,0 +1,8 @@
+package com.luxoft.currencytradeapp.exceptions;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Январь 01 2017
+ */
+public class ExchangeRateNotFoundException extends Exception {
+}
